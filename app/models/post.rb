2503eed38477e4241e62e90862b33384cc49e074
Wes Schierenbeck <wschierenbeck@gmail.com>
2021-12-14 12:06:10 -0500

    commit tree 8d2fc79add95dd16994a5978f9e810f083c231b6

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+  validates :title, presence: true
+
+  #   validate :is_clickbait
+
+  #   def is_clickbait
+  #     unless ["Won't Believe", 'Secret', 'Top', 'Guess'].include?(title)
+  #       errors.add(:title, 'Not clickbaity enough')
+  #     end
+  #   end
+
+  #   def is_clickbait
+  #     unless title.include?("Won't Believe") || title.include?('Secret') ||
+  #              title.include?('Top') || title.include?('Guess')
+  #       errors.add(:title, 'Not clickbaity enough')
+  #     end
+  #   end
 end
